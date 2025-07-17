2589b2d4fb1f11ff98ebc68594542f0d192679a1
Quentin Puig <qpuig@c3r1p7.42perpignan.fr>
2025-07-17 16:27:53 +0200

    all ASM ok

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,15 +1,15 @@
 section .data
-c: db "section .data%1$cc: db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$c%4$cextern _printf%1$c%4$cglobal _main%1$c%1$c;useless comment outside%1$cfunc:%1$c%4$cret%1$c%1$c_main:%1$c%4$c;useless comment inside%1$c%4$cpush rbp%1$c%1$c%4$ccall func%1$c%4$cmov rdi, c%1$c%4$cmov rsi, 10%1$c%4$cmov rdx, 34%1$c%4$cmov rcx, c%1$c%4$cmov r8, 9%1$c%4$cxor rax, rax%1$c%4$ccall _printf%1$c%1$c%4$cpop rbp%1$c%1$c%4$cmov rax, 0%1$c%4$cret%1$c", 0
+c: db "section .data%1$cc: db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$c%4$cextern printf%1$c%4$cglobal main%1$c%1$c;useless comment outside%1$cfunc:%1$c%4$cret%1$c%1$cmain:%1$c%4$c;useless comment inside%1$c%4$cpush rbp%1$c%1$c%4$ccall func%1$c%4$cmov rdi, c%1$c%4$cmov rsi, 10%1$c%4$cmov rdx, 34%1$c%4$cmov rcx, c%1$c%4$cmov r8, 9%1$c%4$cxor rax, rax%1$c%4$ccall printf%1$c%1$c%4$cpop rbp%1$c%1$c%4$cmov rax, 0%1$c%4$cret%1$c", 0
 
 section .text
-	extern _printf
-	global _main
+	extern printf
+	global main
 
 ;useless comment outside
 func:
 	ret
 
-_main:
+main:
 	;useless comment inside
 	push rbp
 
@@ -20,7 +20,7 @@ _main:
 	mov rcx, c
 	mov r8, 9
 	xor rax, rax
-	call _printf
+	call printf
 
 	pop rbp
 
--- ASM/Grace.s
+++ ASM/Grace.s
@@ -1,5 +1,5 @@
 %define NAME "Grace_kid.s", 0
-%define CHAR "%%define NAME %1$cGrace_kid.s%1$c, 0%2$c%%define CHAR %1$c%3$s%1$c, 0%2$c%%macro FT 0%2$csection .data%2$cname: db NAME%2$cflag: db %1$cw%1$c, 0%2$cchar: db CHAR%2$c%2$csection .text%2$c%4$cglobal _main%2$c%4$cextern _fopen%2$c%4$cextern _fclose%2$c%4$cextern _fprintf%2$c%2$c_main:%2$c%4$cpush rbp%2$c%4$cpush rbx%2$c%4$csub rsp, 8%2$c%2$c%4$cmov rdi, name%2$c%4$cmov rsi, flag%2$c%4$cxor rax, rax%2$c%4$ccall _fopen%2$c%2$c%4$ccmp rax, 0%2$c%4$cje return%2$c%2$c%4$cmov rbx, rax%2$c%4$cmov rdi, rax%2$c%4$cmov rsi, char%2$c%4$cmov rdx, 34%2$c%4$cmov rcx, 10%2$c%4$cmov r8, char%2$c%4$cmov r9, 9%2$c%4$ccall _fprintf%2$c%2$c%4$cmov rdi, rbx%2$c%4$ccall _fclose%2$c%2$c%4$cmov rax, 0%2$creturn:%2$c%4$cadd rsp, 8%2$c%4$cpop rbx%2$c%4$cpop rbp%2$c%4$cret%2$c%%endmacro%2$c;useless comment%2$cFT%2$c", 0
+%define CHAR "%%define NAME %1$cGrace_kid.s%1$c, 0%2$c%%define CHAR %1$c%3$s%1$c, 0%2$c%%macro FT 0%2$csection .data%2$cname: db NAME%2$cflag: db %1$cw%1$c, 0%2$cchar: db CHAR%2$c%2$csection .text%2$c%4$cglobal main%2$c%4$cextern fopen%2$c%4$cextern fclose%2$c%4$cextern fprintf%2$c%2$cmain:%2$c%4$cpush rbp%2$c%4$cpush rbx%2$c%4$csub rsp, 8%2$c%2$c%4$cmov rdi, name%2$c%4$cmov rsi, flag%2$c%4$cxor rax, rax%2$c%4$ccall fopen%2$c%2$c%4$ccmp rax, 0%2$c%4$cje return%2$c%2$c%4$cmov rbx, rax%2$c%4$cmov rdi, rax%2$c%4$cmov rsi, char%2$c%4$cmov rdx, 34%2$c%4$cmov rcx, 10%2$c%4$cmov r8, char%2$c%4$cmov r9, 9%2$c%4$ccall fprintf%2$c%2$c%4$cmov rdi, rbx%2$c%4$ccall fclose%2$c%2$c%4$cmov rax, 0%2$creturn:%2$c%4$cadd rsp, 8%2$c%4$cpop rbx%2$c%4$cpop rbp%2$c%4$cret%2$c%%endmacro%2$c;useless comment%2$cFT%2$c", 0
 %macro FT 0
 section .data
 name: db NAME
@@ -7,12 +7,12 @@ flag: db "w", 0
 char: db CHAR
 
 section .text
-	global _main
-	extern _fopen
-	extern _fclose
-	extern _fprintf
+	global main
+	extern fopen
+	extern fclose
+	extern fprintf
 
-_main:
+main:
 	push rbp
 	push rbx
 	sub rsp, 8
@@ -20,7 +20,7 @@ _main:
 	mov rdi, name
 	mov rsi, flag
 	xor rax, rax
-	call _fopen
+	call fopen
 
 	cmp rax, 0
 	je return
@@ -32,10 +32,10 @@ _main:
 	mov rcx, 10
 	mov r8, char
 	mov r9, 9
-	call _fprintf
+	call fprintf
 
 	mov rdi, rbx
-	call _fclose
+	call fclose
 
 	mov rax, 0
 return:
